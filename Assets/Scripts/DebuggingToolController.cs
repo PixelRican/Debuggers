fa70d6ea8420f1a8ca396942a526c1834497c51a
Roberto I. Mercado <rimercado02@gmail.com>
2026-03-19 13:05:28 -0400

    Add laser visual effect

--- Assets/Scripts/DebuggingToolController.cs
+++ Assets/Scripts/DebuggingToolController.cs
@@ -4,7 +4,8 @@ using UnityEngine.InputSystem;
 public sealed class DebuggingToolController : MonoBehaviour
 {
     private const string ActionPath = "XRI Right Interaction/Activate";
-    
+
+    [SerializeField] private GameObject laserPrefab;
     [SerializeField] private InputActionAsset action;
     [SerializeField] private float strength;
 
@@ -21,10 +22,22 @@ public sealed class DebuggingToolController : MonoBehaviour
     private void OnPerformed(InputAction.CallbackContext obj)
     {
         Transform transform = this.transform;
+        Vector3 start = transform.position;
+        Vector3 end;
 
-        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit))
+        if (Physics.Raycast(start, transform.forward, out RaycastHit hit))
         {
-            Destroy(hit.transform.gameObject);
+            end = hit.point;
         }
+        else
+        {
+            end = start + transform.forward * 100.0f;
+        }
+
+        GameObject laser = Instantiate(laserPrefab, (start + end) * 0.5f, transform.rotation * Quaternion.Euler(90, 0, 0));
+        Vector3 scale = laser.transform.localScale;
+        scale.y = (start - end).magnitude * 0.5f;
+        laser.transform.localScale = scale;
+        Destroy(laser, 0.1f);
     }
 }
