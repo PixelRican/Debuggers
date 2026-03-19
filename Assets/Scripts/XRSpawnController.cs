54e7518519bfb7a6f14b05968b261da023cf3992
PixelRican <rimercado02@gmail.com>
2026-03-19 17:44:42 -0400

    Add player spawn controller

---
+++ Assets/Scripts/XRSpawnController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using Unity.XR.CoreUtils;
+using UnityEngine;
+
+public class XRSpawnController : MonoBehaviour
+{
+    [SerializeField] private XROrigin origin;
+    [SerializeField] private Vector3 offset;
+
+    private IEnumerator Start()
+    {
+        yield return null;
+        yield return null;
+
+        Transform originTransform = origin.transform;
+        Vector3 originPosition = originTransform.position;
+        Vector3 headOffset = origin.Camera.transform.position - originPosition;
+        originTransform.position = offset - headOffset;
+        Destroy(this);
+    }
+}
