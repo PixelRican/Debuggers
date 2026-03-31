45d0cda530005f3fbb2f20402d2cda36cfe8d4c2
PixelRican <rimercado02@gmail.com>
2026-03-31 18:21:55 -0400

    Implement erromite wave progression

--- Assets/Scripts/ErromiteWaveController.cs
+++ Assets/Scripts/ErromiteWaveController.cs
@@ -1,13 +1,18 @@
+using System;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 public class ErromiteWaveController : MonoBehaviour
 {
-    [SerializeField] private Transform[] spawnLocations;
-    [SerializeField] private GameObject[] erromitePrefabs;
+    [SerializeField] private WaveEntry[] waves;
+    private WaveEntry currentWave;
+    private int waveIndex;
+    private int spawnIndex;
 
     private void OnEnable()
     {
+        currentWave.Spawns = Array.Empty<SpawnEntry>();
+        waveIndex = -1;
+        spawnIndex = -1;
         InvokeRepeating(nameof(SpawnErromite), 5.0f, 5.0f);
     }
 
@@ -18,8 +23,53 @@ public class ErromiteWaveController : MonoBehaviour
 
     private void SpawnErromite()
     {
-        Transform spawnLocation = spawnLocations[Random.Range(0, spawnLocations.Length)];
-        GameObject erromitePrefab = erromitePrefabs[Random.Range(0, erromitePrefabs.Length)];
-        Instantiate(erromitePrefab, spawnLocation.position, spawnLocation.rotation);
+        WaveEntry wave = currentWave;
+        int nextSpawnIndex = spawnIndex + 1;
+
+        if (nextSpawnIndex >= currentWave.Spawns.Length)
+        {
+            int nextWaveIndex = waveIndex + 1;
+
+            if (nextWaveIndex >= waves.Length)
+            {
+                return;
+            }
+
+            nextSpawnIndex = 0;
+            wave = currentWave = waves[nextWaveIndex];
+            waveIndex = nextWaveIndex;
+        }
+
+        wave.Spawns[nextSpawnIndex].Spawn();
+        spawnIndex = nextSpawnIndex;
+    }
+
+    [Serializable]
+    private struct SpawnEntry
+    {
+        public Transform Location;
+        public GameObject Prefab;
+
+        public SpawnEntry(Transform location, GameObject prefab)
+        {
+            Location = location;
+            Prefab = prefab;
+        }
+
+        public GameObject Spawn()
+        {
+            return Instantiate(Prefab, Location.position, Location.rotation);
+        }
+    }
+
+    [Serializable]
+    private struct WaveEntry
+    {
+        public SpawnEntry[] Spawns;
+
+        public WaveEntry(SpawnEntry[] spawns)
+        {
+            Spawns = spawns;
+        }
     }
 }
