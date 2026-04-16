829c956797052e7faae8f06303a5581ee3d21cda
PixelRican <rimercado02@gmail.com>
2026-04-16 16:58:44 -0400

    Remove unused field

--- Assets/Scripts/ProjectileController.cs
+++ Assets/Scripts/ProjectileController.cs
@@ -3,20 +3,14 @@
 public class ProjectileController : MonoBehaviour
 {
     [SerializeField] private float speed;
-    private Rigidbody _rigidbody;
     private int _damage;
 
     public void SetTarget(GameObject target, int damage)
     {
         Vector3 direction = target.transform.position - transform.position;
         direction.y = 0.0f;
-        _rigidbody.linearVelocity = direction.normalized * speed;
+        GetComponent<Rigidbody>().linearVelocity = direction.normalized * speed;
         _damage = damage;
-    }
-
-    private void Awake()
-    {
-        _rigidbody = GetComponent<Rigidbody>();
     }
 
     private void OnCollisionEnter(Collision other)
